41bf903c375826c2f64d829398559ee0c68a5608
Adam Dennis <adam.dennis@gmail.com>
2024-10-23 17:10:33 -0300

    feature: adding support for React 18

--- assets/js/phoenix_live_react.js
+++ assets/js/phoenix_live_react.js
@@ -1,31 +1,57 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-const render = function(el, target, componentClass, additionalProps = {}, previousProps = {}) {
-  let props = el.dataset.liveReactProps ? JSON.parse(el.dataset.liveReactProps) : {};
+/**
+ * Renders a React component with the given props
+ * @param {HTMLElement} el - The element containing React props
+ * @param {HTMLElement} target - The target element to render the component into
+ * @param {React.ComponentType} componentClass - The React component class
+ * @param {Object} additionalProps - Additional props to pass to the component
+ * @param {Object} previousProps - Previous props for merging
+ * @returns {Object} The final props used for rendering
+ */
+const render = function (
+  el,
+  target,
+  componentClass,
+  additionalProps = {},
+  previousProps = {}
+) {
+  let props = el.dataset.liveReactProps
+    ? JSON.parse(el.dataset.liveReactProps)
+    : {};
   if (el.dataset.liveReactMerge) {
-    props = {...previousProps, ...props, ...additionalProps}
+    props = { ...previousProps, ...props, ...additionalProps };
   } else {
-    props = {...props, ...additionalProps}
+    props = { ...props, ...additionalProps };
   }
   const reactElement = React.createElement(componentClass, props);
-  ReactDOM.render(reactElement, target);
+  if (!target._reactRoot) {
+    target._reactRoot = ReactDOM.createRoot(target);
+  }
+  target._reactRoot.render(reactElement);
   return props;
-}
+};
 
-const initLiveReactElement = function(el, additionalProps) {
+const initLiveReactElement = function (el, additionalProps) {
   const target = el.nextElementSibling;
-  const componentClass = Array.prototype.reduce.call(el.dataset.liveReactClass.split('.'), (acc, el) => { return acc[el] }, window);
+  const componentClass = Array.prototype.reduce.call(
+    el.dataset.liveReactClass.split('.'),
+    (acc, el) => {
+      return acc[el];
+    },
+    window
+  );
   render(el, target, componentClass, additionalProps);
-  return {target: target, componentClass: componentClass};
-}
+  return { target: target, componentClass: componentClass };
+};
 
-const initLiveReact = function() {
-  const elements = document.querySelectorAll('[data-live-react-class]')
-  Array.prototype.forEach.call(elements, el => {
-    initLiveReactElement(el)
+const initLiveReact = function () {
+  const elements = document.querySelectorAll('[data-live-react-class]');
+  Array.prototype.forEach.call(elements, (el) => {
+    initLiveReactElement(el);
   });
-}
+};
 
 const LiveReact = {
   mounted() {
@@ -34,8 +60,12 @@ const LiveReact = {
     const pushEventTo = this.pushEventTo && this.pushEventTo.bind(this);
     const handleEvent = this.handleEvent && this.handleEvent.bind(this);
     const { target, componentClass } = initLiveReactElement(el, { pushEvent });
-    const props = render(el, target, componentClass, { pushEvent, pushEventTo, handleEvent });
-    if (el.dataset.liveReactMerge) this.props = props
+    const props = render(el, target, componentClass, {
+      pushEvent,
+      pushEventTo,
+      handleEvent,
+    });
+    if (el.dataset.liveReactMerge) this.props = props;
     Object.assign(this, { target, componentClass });
   },
 
@@ -45,14 +75,22 @@ const LiveReact = {
     const pushEventTo = this.pushEventTo && this.pushEventTo.bind(this);
     const handleEvent = this.handleEvent;
     const previousProps = this.props;
-    const props = render(el, target, componentClass, { pushEvent, pushEventTo }, previousProps);
-    if (el.dataset.liveReactMerge) this.props = props
+    const props = render(
+      el,
+      target,
+      componentClass,
+      { pushEvent, pushEventTo },
+      previousProps
+    );
+    if (el.dataset.liveReactMerge) this.props = props;
   },
 
   destroyed() {
     const { target } = this;
-    ReactDOM.unmountComponentAtNode(target);
-  }
-}
+    if (target._reactRoot) {
+      target._reactRoot.unmount();
+    }
+  },
+};
 
 export { LiveReact as default, initLiveReact, initLiveReactElement };
