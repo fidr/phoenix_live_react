18f4ad7490b0e11b673c470dc0bd21fab7c97589
Meag Harty <149533950+meagharty@users.noreply.github.com>
2024-11-08 11:44:40 -0500

    deps: upgrade phoenix_html and add phoenix_html_helpers

--- mix.exs
+++ mix.exs
@@ -23,7 +23,8 @@ defmodule PhoenixLiveReact.MixProject do
   defp deps do
     [
       {:ex_doc, "~> 0.21", only: :dev, runtime: false},
-      {:phoenix_html, "~> 2.11 or ~> 3.0"},
+      {:phoenix_html, "~> 4.0"},
+      {:phoenix_html_helpers, "~> 1.0"},
       {:jason, "~> 1.1"}
     ]
   end
